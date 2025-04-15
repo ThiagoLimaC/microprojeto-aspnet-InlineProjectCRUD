11149304e7cbfcba6d9c07136535baf51d9aeced
Thiago Lima <150944198+ThiagoLimaC@users.noreply.github.com>
2025-04-15 15:27:45 -0300

    Add database context to UsuarioController and async Index method
    - Updated `UsuarioController.cs` to include using directives for `InlineProjectCRUD.Data` and `Microsoft.EntityFrameworkCore`.
    - Added a private readonly field `_context` of type `AppDbContext` to `UsuarioController`.
    - Added a constructor to initialize `_context` with an instance of `AppDbContext`.
    - Modified the `Index` action method to be asynchronous and retrieve users from the database using `_context.Usuarios.ToListAsync()`.
    - Added a comment block with a link to more information on enabling MVC for empty projects in `Index.cshtml`.

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InlineProjectCRUD.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace InlineProjectCRUD.Controllers
 {
     public class UsuarioController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+        public UsuarioController(AppDbContext context)
         {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _context.Usuarios.ToListAsync();
             return View();
         }
     }
